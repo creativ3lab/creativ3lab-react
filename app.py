b1c624159e99102a0e230fb294731f17b934f8cc
Andrew Hjellming <71109043+creativ3lab@users.noreply.github.com>
2024-12-08 05:21:13 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+BUILD_PATH = 'C:\Projects\creativ3lab_hosting'
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(BUILD_PATH, 'index.html')
+
+@app.route('/<path:path>')
+def serve_file(path):
+    return send_from_directory(BUILD_PATH, path)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=80)
