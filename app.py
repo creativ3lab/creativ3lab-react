1537ef929e86a8330d91890eff142c4a29d757d8
Andrew Hjellming <71109043+creativ3lab@users.noreply.github.com>
2024-12-08 05:26:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
-BUILD_PATH = 'C:\Projects\creativ3lab_hosting'
+BUILD_PATH = 'C:\\Projects\\creativ3lab_hosting'
 
 @app.route('/')
 def serve_index():
+    """
+    Serves the index.html file from the specified build directory.
+
+    Returns:
+        Response: The response object containing the index.html file.
+    """
     return send_from_directory(BUILD_PATH, 'index.html')
+
 
 @app.route('/<path:path>')
 def serve_file(path):
